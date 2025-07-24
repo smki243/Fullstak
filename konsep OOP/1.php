41c76be983bb0e75a5f5f85746edfc9b4e00f22a
smki243 <smkiu.001@gmail.com>
2025-07-24 13:47:36 +0700

    fullstak

---
+++ konsep OOP/1.php
@@ -0,0 +1,16 @@
+<?php
+class Product
+{
+    public $name;
+    public $price;
+    public $description;
+
+    public function _construct($name, $price, $description)
+    {
+        $this->name = $name;
+        $this->price = $price;
+        $this->description = $description;
+    }    
+}
+
+$product1 = new Product('Smartpone', 5000000, 'smartpone terbaru dengan kamera berkualitas tinggi');
